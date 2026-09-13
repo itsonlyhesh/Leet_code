11a75d483a6e3538bdaf5007fcbe136c8adac460
Heshwanth Reddy Bm <bheshwanth.reddy720@gmail.com>
2026-09-13 20:13:06 +0530

    Time: 71 ms (32.58%), Space: 46.7 MB (51.52%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        List<int[]> list1 = new ArrayList<>();
+        List<int[]> list2 = new ArrayList<>();
+
+        // 1. Gather all coordinates containing 1
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (img1[r][c] == 1) list1.add(new int[]{r, c});
+                if (img2[r][c] == 1) list2.add(new int[]{r, c});
+            }
+        }
+
+        // 2. Count frequencies of each translation vector (dr, dc)
+        Map<Integer, Integer> count = new HashMap<>();
+        int maxOverlap = 0;
+
+        for (int[] p1 : list1) {
+            for (int[] p2 : list2) {
+                int dr = p2[0] - p1[0];
+                int dc = p2[1] - p1[1];
+                // Hash the 2D offset into a unique integer: dr in [-n, n], dc in [-n, n]
+                int key = dr * 100 + dc;
+                int currentCount = count.getOrDefault(key, 0) + 1;
+                count.put(key, currentCount);
+                maxOverlap = Math.max(maxOverlap, currentCount);
+            }
+        }
+
+        return maxOverlap;
+    }
+}
