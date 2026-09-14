c8105eaf096e59263995c5531600dfab46b08d0e
Heshwanth Reddy Bm <bheshwanth.reddy720@gmail.com>
2026-09-14 20:42:46 +0530

    Time: 0 ms (100%), Space: 42.7 MB (52.92%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        // Check if 1D projections overlap on both axes
+        boolean xOverlap = Math.max(rec1[0], rec2[0]) < Math.min(rec1[2], rec2[2]);
+        boolean yOverlap = Math.max(rec1[1], rec2[1]) < Math.min(rec1[3], rec2[3]);
+        
+        return xOverlap && yOverlap;
+    }
+}
